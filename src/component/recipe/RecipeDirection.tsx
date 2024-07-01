0e1c32ad49c358b83582dfc60e053784a3e4bf69
Cinnamon Bun <nguyeney02@gmail.com>
2024-07-01 16:54:48 +0300

    Style RecipeDirection

--- src/component/recipe/RecipeDirection.tsx
+++ src/component/recipe/RecipeDirection.tsx
@@ -1,4 +1,4 @@
-import { GoArrowRight } from "react-icons/go";
+import { FiArrowRight } from "react-icons/fi";
 import { ExtendedRecipe } from "../../common/internal";
 
 interface RecipeDirectionsProps {
@@ -8,15 +8,23 @@ function RecipeDirections({
   selectedRecipe: { sourceUrl, publisher },
 }: RecipeDirectionsProps) {
   return (
-    <div className="bg-slate-200">
-      <h2>How to cook it</h2>
-      <p>
+    <div className="py-12 px-24 flex flex-col items-center">
+      <h2 className="text-xl font-bold text-primary uppercase mb-6 text-center">
+        How to cook it
+      </h2>
+      <p className="text-center mb-9 text-greydark2">
         This recipe was carefully designed and tested by{" "}
-        <span>{publisher}</span>. Please check out directions at their website.
+        <span className="font-bold">{publisher}</span>. Please check out
+        directions at their website.
       </p>
-      <a href={sourceUrl} target="_blank" rel="noreferrer">
+      <a
+        href={sourceUrl}
+        target="_blank"
+        rel="noreferrer"
+        className="text-white bg-gradient-to-br from-background1 to-background2 rounded-full uppercase flex items-center text-sm font-semibold py-3.5 px-6 hover:scale-105"
+      >
         <span>Directions</span>
-        <GoArrowRight />
+        <FiArrowRight className="ml-1.5 h-5 w-5" />
       </a>
     </div>
   );
