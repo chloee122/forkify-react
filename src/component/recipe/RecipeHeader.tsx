85894bb0453982fd855281f1187caf57d51a34fd
Cinnamon Bun <nguyeney02@gmail.com>
2024-07-01 11:49:30 +0300

    Style RecipeHeader

--- src/component/recipe/RecipeHeader.tsx
+++ src/component/recipe/RecipeHeader.tsx
@@ -6,14 +6,14 @@ interface RecipeHeaderProps {
 
 function RecipeHeader({ selectedRecipe }: RecipeHeaderProps) {
   return (
-    <figure className="before:block before:h-100 before:w-100 before:absolute before:bg-slate-100 h-80 relative origin-top">
+    <figure className="h-80 relative before:h-full before:w-full before:bg-gradient-to-br from-background1 to-background2 before:block before:absolute before:top-0 before:left-0 before:opacity-60">
       <img
         className="object-cover block h-full w-full"
         src={selectedRecipe.imageUrl}
         alt={selectedRecipe.title}
       />
-      <h1 className="absolute bottom-0 left-1/2  -translate-x-2/4 translate-y-1/4 -skew-y-6 text-white text-5xl w-1/2 uppercase leading-loose text-center">
-        <span className="py-5 px-8 bg-gradient-to-br from-yellow-100 to-orange-500">
+      <h1 className="absolute bottom-0 left-1/2 -translate-x-2/4 translate-y-1/4 -skew-y-6 text-white text-4xl font-bold uppercase text-center leading-loose">
+        <span className="py-3.5 px-5 bg-gradient-to-br from-background1 to-background2 box-decoration-clone">
           {selectedRecipe.title}
         </span>
       </h1>
